e9f6b6812d95ebdfdcc85987106e3083b86df62a
peppone-choi <peppone.choi@gmail.com>
2024-11-05 09:39:45 +0900

    [최병호] develop 브랜치 수정

--- src/api/common/routerPath.ts
+++ src/api/common/routerPath.ts
@@ -3,21 +3,6 @@ const USER_ROUTES = {
   ADMIN_USER_API: '/admin-api/user',
 } as const;
 
-const PROVINCE_ROUTES = {
-  PROVINCE_API: '/api/province',
-  ADMIN_PROVINCE_API: '/admin-api/province',
-} as const;
-
-const POST_ROUTES = {
-  POST_API: '/api/post',
-  ADMIN_POST_API: '/admin-api/post',
-} as const;
-
-const COMMENT_ROUTES = {
-  COMMENT_API: '/api/comment',
-  ADMIN_COMMENT_API: '/admin-api/comment',
-};
-
 const CATEGORY_ROUTES = {
   CATEGORY_API: '/api/category',
   ADMIN_CATEGORY_API: '/admin-api/category',
@@ -25,8 +10,5 @@ const CATEGORY_ROUTES = {
 
 export const ROUTES_INDEX = {
   ...USER_ROUTES,
-  ...PROVINCE_ROUTES,
-  ...POST_ROUTES,
-  ...COMMENT_ROUTES,
   ...CATEGORY_ROUTES,
 };
