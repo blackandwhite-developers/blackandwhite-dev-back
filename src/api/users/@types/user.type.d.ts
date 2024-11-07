c3e3818092f4c0816a0cf62c9d551b35dae3a82d
peppone-choi <peppone.choi@gmail.com>
2024-11-07 13:39:33 +0900

    [최병호] 유저 타입 수정

--- src/api/users/@types/user.type.d.ts
+++ src/api/users/@types/user.type.d.ts
@@ -4,18 +4,18 @@ type GenderType = 'M' | 'F' | 'E';
 interface IProfile {
   /** ID */
   id: string;
-  /** 이름 */
-  name: string;
   /** 연락처 */
   phone: string;
   /** 생년월일 */
-  birth: string;
+  birth: Date;
   /** 성별 */
   gender?: GenderType;
   /** 주소 */
   address: string;
+  /** 상세 주소 */
+  addressDetail: string;
   /** 관심사 */
-  intereste: string;
+  interest: string;
   /** 닉네임 */
   nickname: string;
 }
@@ -23,12 +23,29 @@ interface IProfile {
 interface IUser {
   /** ID */
   id: string;
+  /** 이름 */
+  name: string;
   /** 이메일  (unique) */
   email: string;
-  /** 비밀번호 */
+  /** 비밀번호  */
   password?: string;
   /** 역할 */
   role: RoleType;
   /** 프로필 */
   profile: IProfile;
+  /** 이용약관 동의 */
+  terms: ITerms;
+}
+
+interface ITerms {
+  /** ID */
+  id: string;
+  /** 이용약관 */
+  termsOfService: boolean;
+  /** 개인정보 수집 / 이용 */
+  privacyPolicy: boolean;
+  /** 위치 정보 이용 동의 (선택) */
+  locationBasedService: boolean;
+  /** 마케팅 정보 수신 동의 (선택) */
+  marketingInfoAgree: boolean;
 }
