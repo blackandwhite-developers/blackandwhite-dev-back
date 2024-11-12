2c257a4490561107f5f5f0371327557d99081f52
peppone-choi <peppone.choi@gmail.com>
2024-11-12 09:55:55 +0900

    [최병호] 소스 수정

--- src/api/users/service/user.service.ts
+++ src/api/users/service/user.service.ts
@@ -17,7 +17,7 @@ export class UsersServiceImpl implements UserService {
 
   async getUsers(): Promise<{ results: GetUsersResponseDTO[] }> {
     const users = await this._mongooseUserRepository.findAll();
-    return { results: users };
+    return { results: users.map(user => new GetUsersResponseDTO(user)) };
   }
 
   async getUserDetail(id: string): Promise<GetUserResponseDTO | null> {
@@ -25,7 +25,7 @@ export class UsersServiceImpl implements UserService {
 
     if (!user) throw new HttpException(404, '유저를 찾을 수 없습니다.');
 
-    const dtoUser = await new GetUserResponseDTO(user);
+    const dtoUser = new GetUserResponseDTO(user);
 
     return dtoUser;
   }
