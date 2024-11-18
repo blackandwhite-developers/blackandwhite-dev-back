2e2f87bfffc5a669c3ff29c4d3f64fcf1d007da4
peppone-choi <peppone.choi@gmail.com>
2024-11-18 11:52:57 +0900

    [최병호] 긴급 패치. phone 확인

--- src/api/users/respository/user/mongooseUser.reopsitory.ts
+++ src/api/users/respository/user/mongooseUser.reopsitory.ts
@@ -51,7 +51,7 @@ export class MongooseUserRepository implements UserRepository {
   }
   /** 이름과 전화번호로 유저 조회 */
   async getEmailByNameAndPhone(email: string, phone: string): Promise<string> {
-    const user = await MongooseUser.findOne({ email, phone });
+    const user = await MongooseUser.findOne({ email }).populate('profile').findOne({ phone });
     if (!user) throw new HttpException(404, '존재 하지 않는 회원입니다.');
     return user.email;
   }
