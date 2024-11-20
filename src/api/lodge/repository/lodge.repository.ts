2c04bbd17cb904b3742a8727103069ef6d3f4700
peppone-choi <peppone.choi@gmail.com>
2024-11-20 10:03:24 +0900

    [최병호] Room 추가 시 Lodge에 추가.

--- src/api/lodge/repository/lodge.repository.ts
+++ src/api/lodge/repository/lodge.repository.ts
@@ -7,4 +7,8 @@ export default interface LodgeRepository {
   edit(id: string, data: Partial<ILodge>): Promise<void>;
   /** 숙소 삭제 */
   delete(id: string): Promise<void>;
+  /** 숙소 객실 추가 */
+  addRoomType(id: string, room: IRoom, count: number): Promise<void>;
+  /** 숙소 객실 재고 수정 */
+  editRoomStock(id: string, roomName: string, stock: number): Promise<void>;
 }
