73744b66a83c99445881f67b96141196c90b5df6
euneunsol <eunsol1882@gmail.com>
2024-11-21 15:53:02 +0900

    [은은솔] lodge랑 room 연결

--- src/api/room/router/adminRoom.router.ts
+++ src/api/room/router/adminRoom.router.ts
@@ -22,7 +22,10 @@ const ADMIN_ROOM_ROUTES = {
 } as const;
 
 const adminRoomController = new AdminRoomController(
-  new RoomServiceImpl(new MongooseRoomRepository(), new MongooseLodgeRepository()),
+  new RoomServiceImpl(
+    new MongooseRoomRepository(), 
+    new MongooseLodgeRepository()
+  ), new MongooseLodgeRepository()
 );
 adminRoomRouter.get(extractPath(ADMIN_ROOM_ROUTES.GET_ROOM, ROUTES_INDEX.ADMIN_ROOMS_API), adminRoomController.getRoom);
 adminRoomRouter.get(
